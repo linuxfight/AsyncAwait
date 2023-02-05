91fea03f904af21a102c03cd76c89d49fac4217a
linuxfight <72513124+linuxfight@users.noreply.github.com>
2023-02-05 17:05:43 +0300

    Initial commit

---
+++ ExampleConsole_01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ExampleConsole_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void Math()
+        {
+
+        }
+    }
+}
