0f5ef4fff67049cc8f4f7212a565e5f684c66d7a
linuxfight <72513124+linuxfight@users.noreply.github.com>
2023-02-05 19:03:09 +0300

    Testing Async

---
+++ AsyncAwait_App/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using AsyncAwait_App.Models;
+
+namespace AsyncAwait_App
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void BtnSync_Click(object sender, RoutedEventArgs e)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            RunSyncLoad();
+            stopwatch.Stop();
+            long time = stopwatch.ElapsedMilliseconds;
+            textBlock.Text += $"Total time: {time}ms\n";
+        }
+
+        public async void BtnAsync_Click(object sender, RoutedEventArgs e)
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            await RunAsyncLoad();
+            stopwatch.Stop();
+            long time = stopwatch.ElapsedMilliseconds;
+            textBlock.Text += $"Total time: {time}ms\n";
+        }
+
+        private async Task RunAsyncLoad()
+        {
+            var sites = PrepareData();
+
+            foreach (var site in sites)
+            {
+                var model = await Task.Run(() => DownloadSite(site));
+                ReportInfo(model);
+            }
+        }
+
+        private void RunSyncLoad()
+        {
+            var sites = PrepareData();
+
+            foreach (var site in sites)
+            {
+                DataModel model = DownloadSite(site);
+                ReportInfo(model);
+            }
+        }
+
+        private void ReportInfo(DataModel dataModel)
+        {
+            textBlock.Text += $"Site: {dataModel.Url}, Length: {dataModel.Data.Length}\n";
+        }
+
+        private List<string> PrepareData()
+        {
+            List<string> list = new List<string>()
+            {
+                "https://vanutp.dev",
+                "https://progtime.net",
+                "https://mootfrost.ru"
+            };
+            return list;
+        }
+
+        private DataModel DownloadSite(string url)
+        {
+            DataModel dataModel = new DataModel();
+            WebClient client = new WebClient();
+
+            string data = client.DownloadString(url);
+            dataModel.Url = url;
+            dataModel.Data = data;
+
+            return dataModel;
+        }
+    }
+}
--- ExampleConsole_01/Program.cs
+++ ExampleConsole_01/Program.cs
@@ -9,22 +9,61 @@ namespace ExampleConsole_01
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            SyncMath();
+            //await AsyncMath();
+            //await AsyncWriteBook();
+            await AsyncAdd();
+
+            Console.WriteLine("Очень жаль");
+            
             Console.ReadLine();
         }
 
-        static void SyncMath()
+        static async Task<int> AsyncAdd()
         {
-            Math();
+            int a = 12;
+            int b = 13;
+            int sum = await Task.Run(() => Add(a, b));
+            //Console.WriteLine(sum);
+            return sum;
+        }
+
+        static int Add(int a, int b)
+        {
+            Console.WriteLine("Start Add");
+            Thread.Sleep(5000);
+            Console.WriteLine("Finish Add");
+
+            return a + b;
+        }
+
+        static async Task AsyncWriteBook()
+        {
+            string text = "Я жду комп уже который день, я ппц устал и просто хочу уже поиграть, ибо я ничего не понимаю и ничего не могу сделать, мне просто хочется отдохнуть.";
+            await Task.Run(() => WriteBook(text));
+            Console.WriteLine("Done");
+        }
+
+        static void WriteBook(string text)
+        {
+            Console.WriteLine("Start WriteBook");
+            Thread.Sleep(5000);
+            Console.WriteLine("Finish WriteBook");
+            Console.WriteLine($"Text: {text}");
+        }
+
+        static async Task AsyncMath()
+        {
+            await Task.Run(Math);
+            Console.WriteLine("Done");
         }
 
         static void Math()
         {
-            Console.WriteLine("Start");
+            Console.WriteLine("Start Math");
             Thread.Sleep(5000);
-            Console.WriteLine("Finish");
+            Console.WriteLine("Finish Math");
         }
     }
 }
